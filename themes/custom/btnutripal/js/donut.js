1b3c043061071503ded18372aca658bcab6b737c
Guillaume Pauli <guillaume@vpsdiglon02>
2018-02-03 14:18:00 +0000

    style du contenu aliments

--- themes/custom/btnutripal/js/donut.js
+++ themes/custom/btnutripal/js/donut.js
@@ -9,7 +9,7 @@
 	      	var fat = parseFloat(drupalSettings.nutripal.values.fat);
 	      	var protein = parseFloat(drupalSettings.nutripal.values.protein);
 	      	var carbohydrate = parseFloat(drupalSettings.nutripal.values.carbohydrate);
-	      	
+
 	        var data = google.visualization.arrayToDataTable([
 	          ['Value', 'percent'],
 	          ['Carbohydrate',    carbohydrate],
@@ -18,16 +18,16 @@
 	        ]);
 
 	        var options = {
-	          title: 'Nutrition Values',
 	          pieHole: 0.4,
 	          slices: {
             	0: { color: 'limegreen' },
             	1: { color: 'firebrick' },
             	2: { color: 'orange' },
-         	  }
+         	  },
+         	  chartArea:{left:0,top:0,width:'100%',height:'100%'}
 	        };
 
-	        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+	        var chart = new google.visualization.PieChart(document.getElementById('nutrition_donut'));
 
 	        chart.draw(data, options);
 	      }

cc60273a1324abfe044746f6a1a5ef939ff25039
Guillaume Pauli <guillaume@vpsdiglon02>
2018-02-03 10:07:57 +0000

    donut js pour les aliments

--- themes/custom/btnutripal/js/donut.js
+++ themes/custom/btnutripal/js/donut.js
@@ -6,22 +6,25 @@
 	      google.charts.setOnLoadCallback(drawChart);
 
 	      function drawChart() {
-
+	      	var fat = parseFloat(drupalSettings.nutripal.values.fat);
+	      	var protein = parseFloat(drupalSettings.nutripal.values.protein);
+	      	var carbohydrate = parseFloat(drupalSettings.nutripal.values.carbohydrate);
+	      	
 	        var data = google.visualization.arrayToDataTable([
 	          ['Value', 'percent'],
-	          ['Protein',     40],
-	          ['Fat',      33],
-	          ['Carbs',    27]
+	          ['Carbohydrate',    carbohydrate],
+	          ['Protein',     protein],
+	          ['Fat',      fat]
 	        ]);
 
 	        var options = {
 	          title: 'Nutrition Values',
 	          pieHole: 0.4,
 	          slices: {
-                0: { color: 'red' },
-                1: { color: 'green' },
-                2: { color: 'blue' },
-              }
+            	0: { color: 'limegreen' },
+            	1: { color: 'firebrick' },
+            	2: { color: 'orange' },
+         	  }
 	        };
 
 	        var chart = new google.visualization.PieChart(document.getElementById('piechart'));

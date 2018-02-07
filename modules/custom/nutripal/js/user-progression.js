4396a1df6606834fdfd1fa592e35341a08b11480
Mo Ya <myanakiev@users.noreply.github.com>
2018-02-07 11:12:30 +0100

    Correct the user-progression if there is target weight and null pointer Exception

--- modules/custom/nutripal/js/user-progression.js
+++ modules/custom/nutripal/js/user-progression.js
@@ -1,42 +1,41 @@
-(function($, Drupal, drupalSettings) {
-  Drupal.behaviors.NutripalBehavior = {
-    attach: function (context, settings) {
-    // can access setting from 'drupalSettings';
-  
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
+(function ($, Drupal, drupalSettings) {
+    Drupal.behaviors.NutripalBehavior = {
+        attach: function (context, settings) {
+            // can access setting from 'drupalSettings';
 
-      function drawChart() {
-        var rows = drupalSettings.nutripal.values.progression;
-        var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Date');
-      data.addColumn('number', 'Weight');
-      data.addColumn('number', 'Target Weight')
+            google.charts.load('current', {'packages': ['corechart']});
+            google.charts.setOnLoadCallback(drawChart);
 
-        console.log(rows);
+            function drawChart() {
+                var rows = drupalSettings.nutripal.values.progression;
+                var data = new google.visualization.DataTable();
+                data.addColumn('string', 'Date');
+                data.addColumn('number', 'Weight');
+                if ("undefined" != typeof rows[0][2]) {
+                    data.addColumn('number', 'Target Weight')
+                }
 
-       var length = rows.length;
-        for (var i = 0; i < length; i++) {
-          /*data.addRows([['2005',  1170]]);*/
+                console.log(rows);
 
-          //console.log(rows[i]);
-          //var date = new Date(rows[i][0] * 1000);
-          data.addRows([rows[i]]);
-          /*data.addRows(
-            [rows[i][0], rows[i][1]]
-          )*/
+                for (var i = 0; i < rows.length; i++) {
+                    //console.log(rows[i]);
+                    //var date = new Date(rows[i][0] * 1000);
+                    data.addRows([rows[i]]);
+                    /*data.addRows(
+                     [rows[i][0], rows[i][1]]
+                     )*/
+                }
+
+                var options = {
+                    title: 'User progression',
+                    curveType: 'function',
+                    legend: {position: 'bottom'}
+                };
+
+                var chart = new google.visualization.LineChart(document.getElementById('line_charts_user'));
+
+                chart.draw(data, options);
+            }
         }
-        
-        var options = {
-          title: 'User progression',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-
-        var chart = new google.visualization.LineChart(document.getElementById('line_charts_user'));
-
-        chart.draw(data, options);
-        }
-     }  
-  }    
+    }
 })(jQuery, Drupal, drupalSettings);
--- modules/custom/nutripal/src/Controller/NutripalUserProgressionController.php
+++ modules/custom/nutripal/src/Controller/NutripalUserProgressionController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Drupal\nutripal\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
@@ -8,37 +9,35 @@ use Drupal\Core\Form\FormInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Database\Statement;
 
-
 class NutripalUserProgressionController extends ControllerBase {
-  public function content(AccountInterface $user, Request $request) {
 
-    $idUser = $user->id();
-    $registeredDate = $user->getCreatedTime();
-    //kint($registeredDate);
-    $weight = $user->field_weight->getString();
-   	$weight_t = $user->field_target_weight->getString();
+    public function content(AccountInterface $user, Request $request) {
 
-    $progression = \Drupal::database()->select('nutripal_user_progression')
-    				->fields('nutripal_user_progression', ['uid', 'weight', 'date'])
-    				->condition('uid', $idUser, '=')
-    				->execute();
+        $idUser = $user->id();
+        $registeredDate = $user->getCreatedTime();
+        //kint($registeredDate);
+        $weight   = isset($user->field_weight)        ? $user->field_weight->getString()        : 0;
+        $weight_t = isset($user->field_target_weight) ? $user->field_target_weight->getString() : 0;
 
-    $rows = [];
-    
-    $rows[] = [\Drupal::service('date.formatter')->format($registeredDate, $format = 'd/m/Y'), $weight, $weight_t];
-    //kint($registeredDate);
-    if($progression) {
+        $progression = \Drupal::database()->select('nutripal_user_progression')
+                ->fields('nutripal_user_progression', ['uid', 'weight', 'date'])
+                ->condition('uid', $idUser, '=')
+                ->execute();
 
-    	foreach ($progression as $value) {
-    		$rows[] = [\Drupal::service('date.formatter')->format($value->date, $format = 'd/m/Y'),$value->weight , $weight_t];
-    	}
+        $rows = [];
+
+        $rows[] = [\Drupal::service('date.formatter')->format($registeredDate, $format = 'd/m/Y'), $weight, $weight_t];
+        //kint($registeredDate);
+        if ($progression) {
+            foreach ($progression as $value) {
+                $rows[] = [\Drupal::service('date.formatter')->format($value->date, $format = 'd/m/Y'), $value->weight, $weight_t];
+            }
+        }
+
+        $headers = array('date', 'weight', 'target weight');
+        $form = \Drupal::formBuilder()->getForm('\Drupal\nutripal\Form\UserProgressionForm');
+
+        return [['#theme' => 'table', '#header' => $headers, '#rows' => $rows], $form];
     }
 
-    $headers = array('date', 'weight', 'target weight');
-
-    $form = \Drupal::formBuilder()->getForm('\Drupal\nutripal\Form\UserProgressionForm');
-
-
-    return [['#theme' => 'table', '#header' => $headers, '#rows' => $rows], $form];
- }
 }
